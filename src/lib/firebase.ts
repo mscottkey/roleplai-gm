a1dd218eee35578b20d6549ebec7e51fa2170d32
Scott Key <mscottkey@gmail.com>
2025-09-28 06:26:04 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Installations: Mis

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,7 +12,7 @@ const firebaseConfig: FirebaseOptions = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREbase_APP_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 }
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
