7e6b29e2595bd70e3800e2f6f6b5ddbc5de99196
Scott Key <mscottkey@gmail.com>
2025-09-29 03:35:48 +0000

    Try fixing this error: `Runtime Error: TypeError: plugin is not a functi

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,9 +1,9 @@
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import {googleAI}from '@genkit-ai/googleai';
 import {enableFirebaseTelemetry} from '@genkit-ai/firebase';
 
 export const ai = genkit({
-  plugins: [googleAI(), enableFirebaseTelemetry()],
+  plugins: [googleAI(), enableFirebaseTelemetry],
   enableTracingAndMetrics: true,
   logLevel: 'debug',
 });
