4f006452cc4c18f41c56958bc904d295029eaef1
Scott Key <mscottkey@gmail.com>
2025-09-29 04:40:16 +0000

    Error: Failed to sanitize request: loadedPlugin.initializer is not a fun

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,7 +3,5 @@ import {googleAI}from '@genkit-ai/googleai';
 import {enableFirebaseTelemetry} from '@genkit-ai/firebase';
 
 export const ai = genkit({
-  plugins: [googleAI(), enableFirebaseTelemetry],
-  enableTracingAndMetrics: true,
-  logLevel: 'debug',
+  plugins: [googleAI(), enableFirebaseTelemetry()],
 });
